1d3145e1bd7dc31380893b50b643be35a638ac99
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-16 20:22:27 +0200

    Init

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
-	def welcome
-		# param[:post]
 
-		# Post.new(post_params.merge(user_id: current_user.id))
-		# current_user.id
-	# 	current_user.posts.create(post_params)
-	# 	post = current_user.posts.build(post_params)
+	def welcome
+
 	 end
+	 
 end
