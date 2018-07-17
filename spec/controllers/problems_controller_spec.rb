69197c38aa1e7b409c6694e18c03d07f62190fe9
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-17 19:39:07 +0200

    Added more tests

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -5,16 +5,23 @@ RSpec.describe ProblemsController, type: :controller do
 
     describe 'template rendering' do 
 
-        let(:author_sud) { create(:author)}
+        let(:user_sud) { create(:user)}
 
         it 'not logged user should be restricted from creating new problems' do 
             get :new_logged_user
             expect(response).to redirect_to(new_user_session_path)
         end
 
-        
+        it 'logged in user should be able to create new post' do 
+            # we confirm the user registration
+            user_sud.confirm
+            sign_in(user_sud)
+
+           # get :new_logged_user
+            #expect(response).to redirect_to(new_user_session_path)
+       
+        end
 
 
-     
     end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     email "example@example.com"
     password "password"
     password_confirmation "password"
-    confirmed_at Date.today
     name "John"
     surname "Kovalsky"
     position "white-collar worker"
