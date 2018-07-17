cde03d33b2bc87bdc63458431c49503febebbee9
Marcin <markiewicz_marcin@protonmail.com>
2018-07-17 15:04:05 +0200

    Adding validation and relation tests

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Problem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 
+	describe 'attributes' do
+		it 'should have proper attributes' do
+			expect(subject.attributes).to include('title', 'content', 'references')
+		end
+	end
+
+	describe 'Validators' do
+			it { should validate_presence_of (:title)}
+			it { should validate_presence_of (:content)}
+			it { should validate_presence_of (:references)}
+
+	end
+
+	describe 'relations' do
+
+		it { should have_many(:problem_users)}
+		it { should have_many(:users)}
+		it { should belong_to(:creator)}
+		
+	end
+
+
+
+
 end
