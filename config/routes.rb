d26221e56b2354aaf52c03653786b1067c99526b
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-17 21:15:18 +0200

    Fixed searching bug

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,16 @@ Rails.application.routes.draw do
   # For logged in user to create new post
   get '/problems/new', to: 'problems#new_logged_user', as: :new_problem_user
   post '/problems', to: 'problems#create'
+
+  # for post searching
+  get '/problems/search_problems', to: 'problems#search_problems', as: :problem_search
+
   get '/problems/:id', to: 'problems#show', as: :problem
 
 
   devise_for :users, :controllers => { registrations: 'registrations'}
 
 
-  # for post searching
-  get '/problems/search_problems', to: 'problems#search_problems', as: :problem_search
+
 
 end
