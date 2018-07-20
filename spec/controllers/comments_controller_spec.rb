ba5bc8e3877a8bfca32f613c37007a1c842860d3
TomKo1 <kottomasz98@gmail.com>
2018-07-20 14:51:32 +0200

    More tests

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+
+RSpec.describe CommentsController, type: :controller do 
+
+    it 'comment should be' do 
+    
+    end
+
+
+
+end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,17 +9,17 @@ RSpec.describe UsersController, type: :controller do
     it { should render_template('index')}
 
     context 'users' do
-    	let!(:user1) { create(:user) }
-		let!(:user2) { create(:user, email:'email2@aa.pl') }
+    	let(:user1) { create(:user) }
+			let(:user2) { create(:user, email:'email2@aa.pl') }
 
-		it 'should return all authors' do
-		expect(assigns(:users)).to match_array([user1,user2])
-		end
+			it 'should return all authors' do
+				expect(assigns(:users)).to match_array([user1,user2])
+			end
     end
   end
 
   describe '#show' do
-  	let!(:user1) { create(:user) }
+  	let(:user1) { create(:user) }
   	before { get :show, params: {id: user1.id} }
 
   	it { should respond_with(200..300)}
@@ -28,6 +28,14 @@ RSpec.describe UsersController, type: :controller do
   	context 'user' do 
   		it { expect(assigns(:user)).to eq(user1) }
   	end 
-  end
+	end
+	
+	describe '#edit' do 
+			let(:user1) { create(:user) }
+
+			 it 'user variable should be present' do 
+				pending "implement #{__FILE__}"
+		 	end
+	end
 
 end
