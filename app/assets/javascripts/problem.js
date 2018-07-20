d863da7d3a88f8c39c5bd471c1a291e9e4b0f538
TomKo1 <kottomasz98@gmail.com>
2018-07-20 10:06:00 +0200

    Init

---
+++ app/assets/javascripts/problem.js
@@ -0,0 +1,13 @@
+// wait for turbolinks to load
+document.addEventListener("turbolinks:load", function() {
+    (function() {
+            $(function() {
+              return $('.chosen-select').chosen({
+                allow_single_deselect: true,
+                no_results_text: 'No results matched',
+                width: '200px'
+            });
+        });
+          
+    }).call(this);
+}); 
