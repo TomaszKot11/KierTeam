2fe267b2e861880adc8572dc1b85dd99bde74142
TomKo1 <kottomasz98@gmail.com>
2018-07-20 10:23:33 +0200

    Added seeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeds for Tags
+Tag.create(name: 'Android')
+Tag.create(name: 'iOS')
+Tag.create(name: 'Ruby on Rails')
+Tag.create(name: 'Spring')
