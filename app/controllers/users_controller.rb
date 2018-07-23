6d53f39bb1c709c3fe104a0edb919939448f212c
TomKo1 <kottomasz98@gmail.com>
2018-07-23 15:31:11 +0200

    Cleaned UsersController code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
-  
   def index
     @users = User.all
   end
 
   def edit
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
-    @problems=Problem.where(creator_id:params[:id]).find_each	 
+    @problems = Problem.where(creator_id: params[:id]).find_each
     @user_img = @user.avatar.url(:thumb)
   end
 end
