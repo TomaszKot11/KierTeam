2fc114c1ceb27ee00e9326f0ff92403775d0d9c7
TomKo1 <kottomasz98@gmail.com>
2018-07-23 15:50:42 +0200

    Cleaned User model

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -8,7 +8,7 @@ class Problem < ApplicationRecord
     has_many :comments, dependent: :destroy
 
     has_many :problem_users, inverse_of: :problem
-    has_many :users, through: :problem_users
+    has_many :users, through: :problem_users, dependent: :destroy
 
     has_many :problem_tags
     has_many :tags, through: :problem_tags
@@ -16,13 +16,13 @@ class Problem < ApplicationRecord
 
     accepts_nested_attributes_for :problem_users
 
-    # checks whether the current logged user is contributor to showed 
+    # checks whether the current logged user is contributor to showed
     # problem and can add comments
     def current_user_contributor?
       contributors = @problem.users
         if @current_user.nil?
           return false
-        else 
+        else
           return @contributors.inlude?(current_user)
         end
     end
@@ -31,6 +31,6 @@ class Problem < ApplicationRecord
     def creator?
       return false if current_user.nil?
       return true if (  current_user.id == self.creator_id )
-      false 
+      false
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,45 +1,45 @@
 class User < ApplicationRecord
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates_attachment :avatar, size: {in: 0..3.megabytes}
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
+  validates_attachment :avatar, size: { in: 0..3.megabytes }
 
   validates :name, :surname, :position, presence: true
-  validates :name, :surname, :position, length: {in: 3..80}
-  
+  validates :name, :surname, :position, length: { in: 3..80 }
+
   has_many :comments, dependent: :destroy
-  has_many :created_problems, foreign_key: :creator_id, class_name: 'Problem', dependent: :destroy
+  has_many :created_problems, foreign_key: :creator_id, class_name: 'Problem', inverse_of: :users
   has_many :problem_users
-  has_many :problems, through: :problem_users
+  has_many :problems, through: :problem_users, dependent: :destroy
 
   after_create :random_avatar, if: :no_avatar
 
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  as_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  as_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }
 
   def full_name
-      "#{name} #{surname}"
-  end 
-  
-  private 
-    # generates default (Gmail-alike) avater 
-    def random_avatar
-      file = Tempfile.new([self.fullname, ".jpg"])
-      file.binmode
-      file.write(Avatarly.generate_avatar(self.name, format: "jpg", size: 300))
-      file.read 
-      begin
-        self.avatar = File.open(file.path)
-      ensure
-        file.close
-        file.unlink
-      end
-      self.save 
-    end
+    "#{name} #{surname}"
+  end
 
-    # checks if avatar was set by user 
-    def no_avatar
-      self.avatar_file_name.nil?
+  private
+
+  # generates default (Gmail-alike) avater
+  def random_avatar
+    file = Tempfile.new([full_name, '.jpg'])
+    file.binmode
+    file.write(Avatarly.generate_avatar(name, format: 'jpg', size: 300))
+    file.read
+    begin
+      self.avatar = File.open(file.path)
+    ensure
+      file.close
+      file.unlink
     end
+    save
+  end
+
+  # checks if avatar was set by user
+  def no_avatar
+    avatar_file_name.nil?
+  end
 end
-
