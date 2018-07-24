7c1b1ee986eb1996416c517ef30202f97419c8a6
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-25 00:06:50 +0200

    Cleaned code

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -26,49 +26,44 @@ class ProblemsController < ApplicationController
   end
 
   def search_problems
+    # for only tag searching searching phrase is not necessary
     if params[:advanced_search_on].present?
-      @problems = perform_advanced_search().paginate(per_page: 5, page: params[:page])
+      @problems = perform_advanced_search || Problem.none
+      @problems = @problems.paginate(per_page: 5, page: params[:page])
     else
-      redirect_to root_path, alert: 'Searching query should not be blank!' unless params[:lookup].present?
+      is_lookup = params[:lookup].present?
+      redirect_to root_path, alert: 'Searching query should not be blank!' unless is_lookup
       @problems = Problem.default_search(params[:lookup]).paginate(per_page: 5, page: params[:page])
     end
   end
 
+  # may be inefficient? - all records?
+  # long if not to show all records when
+  # none option is specified
   def perform_advanced_search
-    problems_loc = Problem.where(nil)
+    is_title = params[:title_on].present?
+    is_content = params[:content_on].present?
+    is_lookup = params[:lookup].present?
 
-    if params[:tag_names].present?
-      params[:tag_names].each do |tag_name|
-        # AND
-        debug('szukam po tagach')
-        problems_loc = problems_loc.tag_where(tag_name)
-      end
-    end
+    problems_loc = tag_search_without_query
 
-    # problems_loc = problems_loc.content_where()
-    if params[:content_on].present? || params[:title_on].present?
-      redirect_to root_path, alert: 'Searching query should not be blank!' unless params[:lookup].present?
-       # content
-      if params[:content_on].present?
-        debug('szukam w content')
-        problems_loc = problems_loc.content_where(params[:lookup])
-      end
+    if is_content || is_title
+      redirect_to root_path, alert: 'Searching query should not be blank!' unless is_lookup
+      # content
+      problems_loc = problems_loc.content_where(params[:lookup]) if is_content
 
       # title
-      if params[:title_on].present?
-        debug('szukam w title')
-        problems_loc = problems_loc.title_where(params[:lookup])
-      end
+      problems_loc = problems_loc.title_where(params[:lookup]) if is_title
     end
-      return problems_loc
+    problems_loc
   end
 
-  def debug(name)
-    p '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    p '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    p name
-    p '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    p '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+  # Tags search is specific beacuse doesn't need any quey to be present
+  def tag_search_without_query
+    tag_names = params[:tag_names].present?
+    problems_loc = Problem.where(nil)
+    params[:tag_names].each { |tag_name| problems_loc = Problem.tag_where(tag_name) } if tag_names
+    problems_loc
   end
 
   def show
