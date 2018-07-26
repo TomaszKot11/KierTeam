54d3857dbde6b6a806a3cc186e5c37e62d962294
TomKo1 <kottomasz98@gmail.com>
2018-07-27 00:47:04 +0200

    Another capistrano fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -74,12 +74,6 @@ namespace :deploy do
     end
   end
 
-namespace :deploy do
-  desc "reload the database with seed data"
-    task :seed do
-      run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-    end
-
     after :publishing, :restart
   end
 
