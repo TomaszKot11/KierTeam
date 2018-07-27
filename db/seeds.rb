e258a005edfa2643ff69999be49ad6780b3d0327
TomKo1 <kottomasz98@gmail.com>
2018-07-27 10:08:50 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,34 @@
 require 'faker'
-# Seeds for Tags
-Tag.create(name: 'Android')
-Tag.create(name: 'iOS')
-Tag.create(name: 'Ruby on Rails')
-Tag.create(name: 'Spring')
 
-# Seeds for admins
 User.create(email:'admin@aa.pl',name:'admin',surname:'admin',password:'admin',position:'admin',is_admin:true).confirm
 
-#Seeds for Problems
-for i in 0..500
- Problem.create(title: Faker::GreekPhilosophers.quote, content: Faker::GreekPhilosophers.name, reference_list: Faker::LordOfTheRings.character, creator_id: User.last.id, status: true, created_at: Faker::Date.between(2.months.ago, Date.today))
+case Rails.env
+when 'development'
+  Tag.create(name: 'Android')
+  Tag.create(name: 'iOS')
+  Tag.create(name: 'Ruby on Rails')
+  Tag.create(name: 'Spring')
+
+  #Seeds for Problems
+  for i in 0..500
+  Problem.create(title: Faker::GreekPhilosophers.quote, content: Faker::GreekPhilosophers.name, reference_list: Faker::LordOfTheRings.character, creator_id: User.last.id, status: true, created_at: Faker::Date.between(2.months.ago, Date.today))
+  end
+when 'production'
+  Tag.create(name: 'Android')
+  Tag.create(name: 'iOS')
+  Tag.create(name: 'Ruby on Rails')
+  Tag.create(name: 'Spring')
+
+  for i in 0..10
+    Problem.create(title: "Android #{i}", content: "Problem with Android #{i}", reference_list: 'Android references', status: true, created_at: Date.today)
+  end
+
+  for i in 0..10
+    Problem.create(title: "iOS #{i}", content: "Problem with iOS #{i}", reference_list: 'iOS references', status: true, created_at: Date.today)
+  end
+
+  for i in 0..10
+    Problem.create(title: "Ruby on Rails #{i}", content: "Problem with Ruby on Rails #{i}", reference_list: 'Ruby on Rails references', status: true, created_at: Date.today)
+  end
 end
+
