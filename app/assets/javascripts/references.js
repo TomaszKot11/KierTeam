af1fa5e473eb2cd0942ec551668af26c5e1b0c46
TomKo1 <kottomasz98@gmail.com>
2018-07-27 21:33:58 +0200

    Basic references logic

---
+++ app/assets/javascripts/references.js
@@ -0,0 +1,106 @@
+document.addEventListener("turbolinks:load", function() {
+    // makes some assumptions
+    // checks the format hwen user stops
+    // typing
+    var typingTimer;
+    // relatively short because programmers
+    // type fast :)
+    var doneTypingInterval = 1500;
+    var $input = $('.references-js');
+
+    // start counting
+    $input.keyup(function(){
+        clearTimeout(typingTimer);
+        if($input.val()){
+            typingTimer = setTimeout(checkFormat, doneTypingInterval);
+        }
+    });
+
+    function checkFormat(){
+        var text = $input.val().trim();
+       //. $input.val
+        split = text.split(/[\r\n]+/);
+      //  alert($input);
+        // remove the potentially empty line
+        // one line
+        if(split.size == 0){
+           // alert('errors -1');
+            makeRedBackground();
+            return 1;
+        }else if(split.size == 1){
+            array = s.split(" ");
+            if(array.size == 1){
+                if(!isValidURL(array[0])){
+                    //alert('errors -2');
+                    makeRedBackground();
+                }else if(array.size == 2){ //!!!!!
+                    if(!isValidURL(array[0])){
+                       // alert('errors -3');
+                       makeRedBackground();
+                    }
+                }else{
+                    // more than 2 words
+                   // alert('errors -4');
+                   makeRedBackground();
+                }
+            }
+            return 1;
+        }
+        if(split[split.length -1] == "") split.pop();
+       for (s of split) {
+          // alert(s);
+        s.trim();
+        array = s.split(" ");
+        // alert(array[0]);
+        if (array.size > 2 || array.size < 1 ){
+            // wrong
+           // alert('errors 1');
+             makeRedBackground();
+            return 1;
+        }else if(array.size == 1){
+            // only URL
+            if(!isValidURL(array[0])){
+                // red
+              //  alert('errors 2');
+                makeRedBackground();
+                return 1;
+            }
+        }else{
+            // both url and name
+            if(!isValidURL(array[0])){
+                // red
+                //alert('errors 3');
+                makeRedBackground();
+                return 1;
+            }
+        }
+
+        }
+
+        makeGreenBackground();
+      }
+
+      function makeRedBackground(){
+        $input.css('background-color','#d9534f');
+      }
+
+      function makeGreenBackground(){
+        $input.css('background-color','#5cb85c');
+      }
+
+    function isValidURL(str) {
+        var pattern = new RegExp('^((https?:)?\\/\\/)?'+ // protocol
+            '(?:\\S+(?::\\S*)?@)?' + // authentication
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locater
+        if (!pattern.test(str)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,20 @@
 module ApplicationHelper
-
-
+  # returns array of links
   def custom_reference_problem(ref_name)
-    # dodac nowe linie
-    splitted = ref_name.split(' ')
-
-    if splitted.count == 1
-      token = make_default_name(splitted[0])
-      get_rid_of_http(splitted[0])
-      return link_to token, "http://#{splitted[0]}"
-    else
-      get_rid_of_http(splitted[0])
-      return link_to splitted[1], "http://#{splitted[0]}"
+    link_name = ref_name.split("\n")
+    links = []
+    link_name.each do |pair|
+      links << produce_link(pair)
     end
+    return links
   end
 
   private
 
-  def make_default_name(string_tokenize)
-    return '' if string_tokenize.blank?
-    return string_tokenize.split('@').last if string_tokenize.match('@')
-    link = string_tokenize
-    link = "http://#{link}" unless link.match(/^(http:\/\/|https:\/\/)/)
+  def extract_domain_name(raw_link)
+    return '' if raw_link.blank?
+    link = raw_link
+    link = "http://#{raw_link}" unless raw_link.match(/^(http:\/\/|https:\/\/)/)
     link = URI.parse(URI.encode(link)).host.present? ? URI.parse(URI.encode(link)).host : link.strip
     domain_name = link.sub(/.*?www./,'')
     domain_name = domain_name.match(/[A-Z]+.[A-Z]{2,4}$/i).to_s if domain_name.split('.').length >= 2 && domain_name.match(/[A-Z]+.[A-Z]{2,4}$/i).present?
@@ -30,6 +23,21 @@ module ApplicationHelper
   def get_rid_of_http(string_clean)
     string_clean.remove!('http://')
     string_clean.remove!('https://')
+  end
+
+  def produce_link(link_name)
+    split = link_name.split(' ')
+    token = 'REFERENCE'
+    if split.count == 1
+      # user didn't provide name
+      token = extract_domain_name(split[0])
+      get_rid_of_http(split[0])
+    else
+      # user provided name
+      get_rid_of_http(split[0])
+      token = split[1]
+    end
+    return link_to token, "http://#{split[0]}"
   end
 
 end
