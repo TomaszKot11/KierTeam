a18d134b367a81e22ec6c410d05b24ef0c6d1235
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-28 16:08:42 +0200

    Fixed rubocop issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-   # flash[:notice] = 'User was destroyed!'
+    # flash[:notice] = 'User was destroyed!'
   end
 
   def edit
@@ -22,5 +22,4 @@ class UsersController < ApplicationController
     @problems_not_posted = user_problems.where(status: false)
     @user_img = @user.avatar.url(:thumb)
   end
-
 end
