462fea7c1c262840e49f985753943d96ccd178a2
raffalj <r.jaworowsky@gmail.com>
2018-07-30 14:11:16 +0200

    Fixed tags form in search engine

--- app/assets/javascripts/problem_form_selector.js
+++ app/assets/javascripts/problem_form_selector.js
@@ -5,6 +5,7 @@ document.addEventListener("turbolinks:load", function() {
               return $('.chosen-select').chosen({
                 allow_single_deselect: true,
                 no_results_text: 'No results matched',
+                width:"240px",
             });
         });
 
