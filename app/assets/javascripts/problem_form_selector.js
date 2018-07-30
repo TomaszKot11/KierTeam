a9870092f57d67b705fa4cbb61260cb1f06598db
raffalj <r.jaworowsky@gmail.com>
2018-07-30 14:16:27 +0200

    Search to fix

--- app/assets/javascripts/problem_form_selector.js
+++ app/assets/javascripts/problem_form_selector.js
@@ -5,7 +5,7 @@ document.addEventListener("turbolinks:load", function() {
               return $('.chosen-select').chosen({
                 allow_single_deselect: true,
                 no_results_text: 'No results matched',
-                width:"240px",
+                width:"100%",
             });
         });
 
