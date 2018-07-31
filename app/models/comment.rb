a2cf08225fed1aeb93afe05f3647d483e2a3a413
raffalj <r.jaworowsky@gmail.com>
2018-07-31 15:25:00 +0200

    Fixed styling, content of the comment and problem is increased to 1500 characters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   validates :title, :content, presence: true
-  validates :title, length: { in: 5..80 }
-  validates :content, length: { in: 5..500 }
+  validates :title, length: { in: 5..160 }
+  validates :content, length: { in: 5..1500 }
   belongs_to :problem
   belongs_to :user
 end
