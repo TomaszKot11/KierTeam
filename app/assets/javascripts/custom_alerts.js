75c334f61d20f8644f53da6f64d15752e45582fc
TomKo1 <kottomasz98@gmail.com>
2018-08-01 10:09:01 +0200

    Added custom alert dialog

---
+++ app/assets/javascripts/custom_alerts.js
@@ -0,0 +1,24 @@
+$(function() {
+    window.alert = function(msg) {
+        $(this.document.createElement('div')).html(msg)
+            .dialog({
+                buttons: [
+                    {
+                    text: 'OK',
+                    click: function() {$(this).dialog('close');},
+                    class: 'btn btn-success'
+                    },
+                    {
+                        text: 'Cancel',
+                        click: function(){$(this).dialog('close');console.log('Easter egg! :)');},
+                        class: 'btn btn-danger'
+                    }
+                ],
+                modal: true,
+                width: 'auto',
+                resizable: false,
+                title: 'Warning',
+                draggable: true
+            });
+    }
+});
