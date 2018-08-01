120951ecfbfb78cf66e518df557b853ee24cdd6a
TomKo1 <kottomasz98@gmail.com>
2018-08-01 11:53:06 +0200

    Removed confirmation dialog

--- app/assets/javascripts/custom_alerts.js
+++ app/assets/javascripts/custom_alerts.js
@@ -22,31 +22,4 @@ $(function() {
                 draggable: true
             });
     };
-
-    window.confirm = function(msg) {
-        $(this.document.createElement('div')).html(msg).dialog({
-            buttons: [
-                {
-                text: 'OK',
-                click: function() {
-                    $(this).dialog('close');
-                    return true;
-                },
-                class: 'btn btn-success'
-                },
-                {
-                    text: 'Cancel',
-                    click: function(){
-                        $(this).dialog('close');
-                        return false;
-                    },
-                    class: 'btn btn-danger'
-                }
-            ],
-            draggable: false,
-            modal: true,
-            resizable: false,
-            width: 'auto'
-        });
-    }
 });
