4065379703ad016a91c0141b1cf738cd9fa9f689
raffalj <r.jaworowsky@gmail.com>
2018-08-02 12:04:24 +0200

    testing basic search done, advanced not finished

--- spec/views/problems_view_spec.rb
+++ spec/views/problems_view_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "scenario - visit page, create problem, destroy problem, edit problem, add comment, destroy comment, search for problem", type: :feature do
+describe "scenario - visit page, create problem, destroy problem, edit problem, add comment, destroy comment", type: :feature do
 
   let!(:user_sud) { create(:user, email: 'user@example.com', password: 'password', is_admin: true) }
   let!(:problem) {create(:problem,title: 'Zaraz przyjdzie wiosna', content: 'Będzie za momencik', reference_list: 'google.com wiosna', creator_id: user_sud.id, status: true)}
@@ -81,23 +81,92 @@ describe "scenario - visit page, create problem, destroy problem, edit problem, 
     page.driver.browser.switch_to.alert.accept
     expect(page).to have_text('Your problem was successfully destroyed!')
   end
+end
 
-  context 'searching engine' do
-    it 'using basic searching engine, see results, see one result' do
-      # laziness
-      problem_a
-      problem_b
-      problem_c
+describe "search engine", type: :feature do
+  let!(:user1) { create(:user, email: 'user1@example.com', password: 'password') }
+  let!(:user2) { create(:user, email: 'user2@example.com', password: 'password') }
+
+  let!(:problem1) {create(:problem, title:'Android not connecting with firebase', content:'In the android configuration, there need to be a line: https-status:true', creator_id: user1.id, status: true)}
+  let!(:problem2) {create(:problem, title:'Spring - mysql not working properly', content:'file mysql.lock must be in the properties directory', creator_id: user1.id, status: true)}
+  let!(:problem3) {create(:problem, title:'C++ SDL not loading music during game running', content:'Additional library is needed', creator_id: user2.id, status: true)}
+
+  let!(:tag1) {create(:tag)}
+  let!(:tag2) {create(:tag)}
+  let!(:tag3) {create(:tag)}
+  let!(:tag4) {create(:tag)}
+
+  let!(:problem1_tag1) { create(:problem_tag, problem_id: problem1.id, tag_id: tag1.id)}
+  let!(:problem1_tag2) { create(:problem_tag, problem_id: problem1.id, tag_id: tag2.id)}
+  let!(:problem1_tag3) { create(:problem_tag, problem_id: problem1.id, tag_id: tag3.id)}
+
+  let!(:problem2_tag1) { create(:problem_tag, problem_id: problem2.id, tag_id: tag1.id)}
+  let!(:problem2_tag2) { create(:problem_tag, problem_id: problem2.id, tag_id: tag3.id)}
+  let!(:problem2_tag3) { create(:problem_tag, problem_id: problem2.id, tag_id: tag4.id)}
+
+  let!(:problem3_tag1) { create(:problem_tag, problem_id: problem3.id, tag_id: tag2.id)}
+  let!(:problem3_tag2) { create(:problem_tag, problem_id: problem3.id, tag_id: tag3.id)}
+
+  context 'Guest - basic search' do
+    before :each do
       visit root_path
-      page.fill_in 'lookup', with: 'Android'
-      within 'form' do
-        find('.searching-btn').click
-      end
-      expect(page).to have_current_path(problem_search_path, ignore_query: true)
     end
 
-    it 'using advanced searching' do
+    it 'Guest can use basic search engine - case 1' do
+      page.fill_in 'lookup', with: problem1.title
+      click_link_or_button 'Search'
+      page.should have_content(problem1.title)
+      page.should have_no_content(problem2.title)
+      page.should have_no_content(problem3.title)
+    end
 
+    it 'Guest can use basic search engine - case 2' do
+      page.fill_in 'lookup', with: problem2.title
+      click_link_or_button 'Search'
+      page.should have_no_content(problem1.title)
+      page.should have_content(problem2.title)
+      page.should have_no_content(problem3.title)
     end
   end
+
+  context 'User - basic search' do
+    before :each do
+      user1.confirm
+      visit '/users/sign_in'
+      within(".new_user") do
+        fill_in 'Email', with: user1.email
+        fill_in 'Password', with: user1.password
+      end
+      click_button 'Log in'
+      visit root_path
+    end
+
+    it 'User can use basic search engine - case 1' do
+      page.fill_in 'lookup', with: problem3.title
+      click_link_or_button 'Search'
+      page.should have_no_content(problem1.title)
+      page.should have_no_content(problem2.title)
+      page.should have_content(problem3.title)
+    end
+
+    it 'User can use basic search engine - case 2' do
+      page.fill_in 'lookup', with: 'asdggsd'
+      click_link_or_button 'Search'
+      page.should have_no_content(problem1.title)
+      page.should have_no_content(problem2.title)
+      page.should have_no_content(problem3.title)
+    end
+  end
+
+  context 'Guest - advanced search' do
+    before :each do
+      visit root_path
+    end
+
+    it 'Guest can use advanced search' do
+      click_link_or_button 'Advanced searching'
+    end
+
+  end
+
 end
